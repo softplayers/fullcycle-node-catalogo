48a026efbb0e6c55205ded5d9ec2b3eb4fc4645b
Renato <accounts01@softplayers.com>
2021-09-15 14:13:42 -0300

    feat: update es connector to v7

--- src/datasources/esv7.datasource.ts
+++ src/datasources/esv7.datasource.ts
@@ -3,12 +3,14 @@ import {juggler} from '@loopback/repository';
 
 const config = {
   name: 'esv7',
-  connector: 'es',
+  connector: 'esv6',
   index: 'catalog',
   apiVersion: '7',
   defaultSize: '',
   configuration: {
-    node: process.env.ELASTIC_SEARCH_HOST
+    node: process.env.ELASTIC_SEARCH_HOST,
+    requestTimeout: process.env.ELASTIC_SEARCH_REQUEST_TIMEOUT,
+    pingTimeout: process.env.ELASTIC_SEARCH_PING_TIMEOUT,
   }
 };
 
